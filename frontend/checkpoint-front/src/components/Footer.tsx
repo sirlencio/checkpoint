3ce01d29df4b51d492a28b0e6c67200cfbc7256a
Nelius72 <113987289+Nelius72@users.noreply.github.com>
2025-10-23 12:01:07 +0200

    layout general hecho

---
+++ frontend/checkpoint-front/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 border-t border-gray-700 p-4 text-center text-sm">
+      <p>© {new Date().getFullYear()} CheckPoint — Creado por The Council 🎮</p>
+    </footer>
+  )
+}
